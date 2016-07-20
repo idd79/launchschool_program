3e6eb69899fbb745967fe69f270625b8e6221b9a
Ivan Duran <idd79@yandex.com>
2016-07-20 21:56:38 +0300

    Refactor and rubocop calculator.rb and lesson 2 notes update

--- 101_Programming_Foundations/lesson2_notes.markdown
+++ 101_Programming_Foundations/lesson2_notes.markdown
@@ -68,3 +68,41 @@ end
 - After checking that the above logic works, we can start improving our code. For example, instead of checking whether `saved_number` is set to `nil` in every iteration, we could just assign `saved_number` to `numbers.first` instead (of `saved_number ||= num`).
 
 - For bigger and more sophisticated problems, we need to take a piecemeal approach to applying pseudo-code, then translating that to Ruby code to verify the logic is correct, then moving on to the next piece in the problem. Step by step, we can slowly load the problem into our brain, verifying the logic each step along the way.
+
+
+## Rubocop
+
+- Install gem with `$ gem install rubocop`
+- Then use rubocop on a file, e.g., `$ rubocop test.rb`
+- In rubocop output, `C` stands for convention, `W` for warning, `E` for error, and `F` for fatal error.
+- We can also use `$ rubocop test.rb --format offenses` to find out which cop complained.
+- We can configure rubocop to, for example, not to enforce certain offenses. Rubocop will look for a `.rubocop.yml` file for configuration settings.
+- Example:
+
+```yaml
+# .rubocop.yml
+
+Style/StringLiterals:
+  Enabled: false
+  EnforcedStyle: single_quotes
+  SupportedStyles:
+  - single_quotes
+  - double_quotes
+```
+
+## Refactoring calculator.rb
+
+- Use `%w(1 2 3 4)` to get an array of strings which is equal to `["1", "2", "3", "4"]`.
+- Other % literals are:
+    - %r() is another way to write a regular expression.
+    - %q() is another way to write a single-quoted string (and can be multi-line, which is useful)
+    - %Q() gives a double-quoted string
+    - %x() is a shell command
+    - %i() gives an array of symbols (Ruby >= 2.0.0)
+    - %s() turns foo into a symbol (:foo)
+- If you need to use a variable outside a `loop` that it's going to be determined inside the `loop`, make sure to initialize that variable outside the loop, e.g., `number = ''`.
+
+
+## Debugging
+
+
--- 101_Programming_Foundations/lesson_2/calculator.rb
+++ 101_Programming_Foundations/lesson_2/calculator.rb
@@ -3,80 +3,158 @@
 # asks for the type of operation to perform: add, subtract, multiply or divide
 # displays the result
 
-
 # My solution
 
-def run
-  loop do
-    puts "\n"
-    puts "Menu:"
-    puts "(a) Add"
-    puts "(b) Substract"
-    puts "(c) Multiply"
-    puts "(d) Divide"
-    puts "(e) Exit"
-    print "Type one of the options from the menu above (i.e. a, b, ..., or e): "
-    ans = gets.chomp
-    case ans
-    when "e"
-      break
-    when "a"
-      puts "The result is #{operate("add")}"
-    when "b"
-      puts "The result is #{operate("substract")}"
-    when "c"
-      puts "The result is #{operate("multiply")}"
-    when "d"
-      operate("divide")
-    end
-  end
-end
+# def run
+#   loop do
+#     puts "\n"
+#     puts "Menu:"
+#     puts "(a) Add"
+#     puts "(b) Substract"
+#     puts "(c) Multiply"
+#     puts "(d) Divide"
+#     puts "(e) Exit"
+#     print "Type one of the options from the menu above
+#           (i.e. a, b, ..., or e): "
+#     ans = gets.chomp
+#     case ans
+#     when "e"
+#       break
+#     when "a"
+#       puts "The result is #{operate("add")}"
+#     when "b"
+#       puts "The result is #{operate("substract")}"
+#     when "c"
+#       puts "The result is #{operate("multiply")}"
+#     when "d"
+#       operate("divide")
+#     end
+#   end
+# end
 
-def operate(method)
-  print "Enter the first number: "
-  x = gets.chomp.to_f
-  print "Enter the second number: "
-  y = gets.chomp.to_f
-  case method
-  when "add"
-    result = x + y
-  when "substract"
-    result = x - y
-  when "multiply"
-    result = x * y
-  when "divide"
-    if y == 0
-      puts "Warning!, divided by 0"
-    else
-      puts "The result is #{x / y}"
-    end
-  end
-end
+# def operate(method)
+#   print "Enter the first number: "
+#   x = gets.chomp.to_f
+#   print "Enter the second number: "
+#   y = gets.chomp.to_f
+#   case method
+#   when "add"
+#     x + y
+#   when "substract"
+#     x - y
+#   when "multiply"
+#     x * y
+#   when "divide"
+#     if y == 0
+#       puts "Warning!, divided by 0"
+#     else
+#       puts "The result is #{x / y}"
+#     end
+#   end
+# end
 
 # run()
 
-
 # Lauchschool solution
 
-puts("Welcome to Calculator")
-
-puts("What's the first number?")
-number1 = gets.chomp
-
-puts("What's the second number?")
-number2 = gets.chomp
-
-puts "What operation would you like to perform? 1) add 2) substract 3) multiply 4) divide"
-operator = gets.chomp
-
-if operator == '1'
-  result = number1.to_i + number2.to_i
-elsif operator == '2'
-  result = number1.to_i - number2.to_i
-elsif operator == '3'
-  result = number1.to_i * number2.to_i
-else
-  result = number1.to_f / number2.to_f
+def prompt(message)
+  Kernel.puts("=> #{message}")
 end
 
-puts "The result is #{result}"
+def valid_number?(num)
+  num.to_i != 0
+end
+
+def operation_to_message(op)
+  case op
+  when '1'
+    'Adding'
+  when '2'
+    'Substracting'
+  when '3'
+    'Multiplying'
+  when '4'
+    'Dividing'
+  end
+end
+
+prompt("Welcome to Calculator! Enter your name:")
+
+name = ''
+
+loop do
+  name = gets.chomp
+
+  if name.empty?()
+    prompt("Make sure to use a valid name.")
+  else
+    break
+  end
+end
+
+prompt("Hi #{name}!")
+
+loop do # main loop
+  number1 = ''
+  loop do
+    prompt("What's the first number?")
+    number1 = gets.chomp
+    if valid_number?(number1)
+      break
+    else
+      prompt("Hmm... that doesn't look like a valid number")
+    end
+  end
+
+  number2 = ''
+  loop do
+    prompt("What's the second number?")
+    number2 = gets.chomp
+    if valid_number?(number2)
+      break
+    else
+      prompt("Hmm... that doesn't look like a valid number")
+    end
+  end
+
+  operator_prompt = <<-MSG
+    What operation would you like to perform?
+    1) add
+    2) substract
+    3) multiply
+    4) divide
+  MSG
+
+  prompt(operator_prompt)
+
+  operator = ''
+  loop do
+    operator = gets.chomp
+
+    if %w(1 2 3 4).include?(operator)
+      break
+    else
+      prompt("Must choose 1, 2, 3 or 4")
+    end
+  end
+
+  prompt("#{operation_to_message(operator)} the two numbers...")
+
+  result = case operator
+           when '1'
+             number1.to_i + number2.to_i
+           when '2'
+             number1.to_i - number2.to_i
+           when '3'
+             number1.to_i * number2.to_i
+           when '4'
+             number1.to_f / number2.to_f
+           end
+
+  prompt("The result is #{result}")
+  prompt("Do you want to perform another calculation? (Y to calculate again)")
+  answer = gets.chomp
+  break unless answer.downcase.start_with?('y')
+end
+
+prompt("Thank you for using the calculator. Good bye!")
