011038ccb0428b561a990e396e12b5a569df6ae8
Ivan Duran <idd79@yandex.com>
2016-08-25 11:22:28 +0300

    Refactor tictactoe game, specially the case statement

--- 101_Programming_Foundations/lesson_4/tictactoe.rb
+++ 101_Programming_Foundations/lesson_4/tictactoe.rb
@@ -1,5 +1,8 @@
 # tictactoe.rb
 
+WINNING_CASES = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7],
+                 [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]].freeze
+
 def display_board(brd)
   puts ""
   puts " #{brd[1]} | #{brd[2]} | #{brd[3]}"
@@ -10,15 +13,10 @@ def display_board(brd)
   puts ""
 end
 
-def winning_cases
-  [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7],
-   [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]]
-end
-
-def almost_win_case(brd, mark = 'X')
+def almost_win_case(brd, mark)
   selection = []
   values = []
-  winning_cases.each do |item|
+  WINNING_CASES.each do |item|
     if brd.values_at(*item).count(mark) == 2 &&
        brd.values_at(*item).count(" ") == 1
       selection = item
@@ -31,18 +29,18 @@ def almost_win_case(brd, mark = 'X')
   [selection, values]
 end
 
-def computer_chooce(current_board, mark = 'X')
+def computer_chooce(current_board, mark)
   selection, values = almost_win_case(current_board, mark)
   selection.empty? ? nil : selection[values.index(" ")].to_s
 end
 
-def win_condition?(brd, mark = 'X')
-  winning_cases.any? do |item|
+def win_condition?(brd, mark)
+  WINNING_CASES.any? do |item|
     brd.values_at(*item).count(mark) == 3
   end
 end
 
-def annotate_board!(hash, input, mark = "X")
+def annotate_board!(hash, input, mark)
   hash[input.to_i] = mark
   hash
 end
@@ -53,7 +51,7 @@ def initialize_board
   new_board
 end
 
-def initial_choices
+def initial_choices_board
   new_board = {}
   (1..9).each { |num| new_board[num] = num.to_s }
   new_board
@@ -63,9 +61,25 @@ def prompt(message)
   puts "=> #{message}"
 end
 
+def clear_system
+  system("clear") || system("cls")
+end
+
 def ask_player(question)
   prompt question
   gets.chomp
+end
+
+def ask_player_loop(question, *options)
+  answer = ''
+  loop do
+    answer = ask_player(question)
+
+    break if answer.downcase.start_with?(*options)
+    clear_system
+    prompt "That's not a valid choice. Try again."
+  end
+  answer
 end
 
 def valid_mark?(answer)
@@ -84,22 +98,70 @@ def add_score!(player, current_score)
   current_score[player.to_sym] += 1
 end
 
-def alternate_player(ply)
-  ply == 'human' ? 'computer' : 'human'
+def alternate_player(player)
+  player == 'human' ? 'computer' : 'human'
 end
 
-def ask_player_loop(variable, question, *options)
-  loop do
-    variable = ask_player(question)
+def prompt_player_won(brd, player)
+  display_board(brd)
+  prompt "#{player} won!"
+end
 
-    break if variable.downcase.start_with?(*options)
-    system 'clear'
-    prompt "That's not a valid choice. Try again."
+def prompt_its_a_tie(brd)
+  clear_system
+  display_board(brd)
+  prompt("Oh! It's a tie.")
+end
+
+def prompt_players_chose(player, choice)
+  prompt "#{player} chose #{choice}"
+  puts ""
+end
+
+def computers_strategy(current_brd, status_brd,
+                       players_marker, computers_marker, level)
+  if !computer_chooce(current_brd, computers_marker).nil? # Winning strategy
+    computer_chooce(current_brd, computers_marker)
+  elsif !computer_chooce(current_brd, players_marker).nil? # Defensive strategy
+    computer_chooce(current_brd, players_marker)
+  elsif level == 'medium' && current_brd[5] == " " # Select 5 strategy
+    "5"
+  else
+    status_brd.values.select { |i| i != " " }.sample # Random strategy
   end
-  variable
 end
 
-system 'clear'
+def human_plays(current_brd, status_brd, mark)
+  prompt "Your turn..."
+  puts ""
+  choice = ""
+  loop do
+    prompt("Current game board: (Your marker is #{mark})")
+    display_board(current_brd)
+    prompt("Choose a number from the board below:")
+    display_board(status_brd)
+    choice = gets.chomp
+
+    break if valid_choice?(status_brd.values, choice)
+    clear_system
+    prompt("You entered a wrong value! Please try again...")
+  end
+  annotate_board!(current_brd, choice, mark)
+  annotate_board!(status_brd, choice, " ")
+end
+
+def computer_plays(brd, status_brd, players_marker, computers_marker, level)
+  computers_choice =
+    computers_strategy(brd, status_brd,
+                       players_marker, computers_marker, level)
+
+  annotate_board!(brd, computers_choice, computers_marker)
+  annotate_board!(status_brd, computers_choice, " ")
+
+  prompt_players_chose('Computer', computers_choice)
+end
+
+clear_system
 puts "Welcome to Tic Tac Toe game"
 
 score = { human: 0, computer: 0 }
@@ -110,33 +172,30 @@ loop do
                              "(choose a number from 1 to 5):")
 
   break if valid_winning_score?(winning_score)
-  system 'clear'
+  clear_system
   prompt("That's not a valid choice.")
 end
 
 initial_player = ''
-initial_player = ask_player_loop(initial_player,
-                                 "Who should play first? (human or computer)",
+initial_player = ask_player_loop("Who should play first? (human or computer)",
                                  'hum', 'com')
 
 initial_player =
   initial_player.downcase.start_with?('hum') ? 'human' : 'computer'
 
 level = ''
-level = ask_player_loop(level,
-                        "Select the level of difficulty: (easy or medium)",
+level = ask_player_loop("Select the level of difficulty: (easy or medium)",
                         'eas', 'med')
 
 level =
   level.downcase.start_with?('eas') ? 'easy' : 'medium'
 
 players_mark = ''
-# Select X or O loop
 loop do
   players_mark = ask_player("Select your marker: 'X' or 'O'")
 
   break if valid_mark?(players_mark)
-  system 'clear'
+  clear_system
   prompt "That's not a valid choice"
 end
 
@@ -147,9 +206,9 @@ computers_mark =
 
 # Main loop
 loop do
-  system 'clear'
+  clear_system
   board = initialize_board
-  status_board = initial_choices
+  status_board = initial_choices_board
 
   prompt "The current score is " \
          "Player: #{score[:human]}/#{winning_score}; " \
@@ -159,81 +218,26 @@ loop do
   current_player = initial_player
   # Inside game loop
   loop do
-    case current_player
-    when 'human'
+    if current_player == 'human'
+      human_plays(board, status_board, players_mark)
+      clear_system
+    else
+      computer_plays(board, status_board, players_mark, computers_mark, level)
+    end
 
-      choice = ""
-      # Player game choice loop
-      loop do
-        prompt "Your turn..."
-        puts ""
-        prompt("Current game board: (Your marker is #{players_mark})")
-        display_board(board)
-        prompt("Choose a number from the board below:")
-        display_board(status_board)
-        choice = gets.chomp
+    current_player = alternate_player(current_player)
 
-        break if valid_choice?(status_board.values, choice)
-        prompt("You entered a wrong value! Please try again...")
-      end
-
-      annotate_board!(board, choice, players_mark)
-      annotate_board!(status_board, choice, " ")
-
-      system 'clear'
-      prompt "You have chosen #{choice}"
-      puts ""
-
-      if win_condition?(board, players_mark)
-        system 'clear'
-        prompt("You have chosen #{choice}")
-        display_board(board)
-        prompt("You won!")
-        add_score!('human', score)
-        break
-      elsif !board.values.include?(" ")
-        system 'clear'
-        prompt("You have chosen #{choice}")
-        display_board(board)
-        prompt("Oh! It's a tie.")
-        break
-      end
-
-      current_player = alternate_player(current_player)
-
-    when 'computer'
-
-      computers_choice =
-        if !computer_chooce(board, computers_mark).nil? # Winning strategy
-          computer_chooce(board, computers_mark)
-        elsif !computer_chooce(board, players_mark).nil? # Defensive strategy
-          computer_chooce(board, players_mark)
-        elsif level == 'medium' && board[5] == " "
-          "5"
-        else
-          status_board.values.select { |i| i != " " }.sample # Random strategy
-        end
-
-      annotate_board!(board, computers_choice, computers_mark)
-      annotate_board!(status_board, computers_choice, " ")
-
-      prompt "Computer has chosen #{computers_choice}"
-      puts ""
-
-      if win_condition?(board, computers_mark)
-        display_board(board)
-        prompt("The computer won!")
-        add_score!('computer', score)
-        break
-      elsif !board.values.include?(" ")
-        system 'clear'
-        prompt("Computer has chosen #{computers_choice}")
-        display_board(board)
-        prompt("Oh! It's a tie.")
-        break
-      end
-
-      current_player = alternate_player(current_player)
+    if win_condition?(board, players_mark)
+      prompt_player_won(board, 'You')
+      add_score!('human', score)
+      break
+    elsif win_condition?(board, computers_mark)
+      prompt_player_won(board, 'Computer')
+      add_score!('computer', score)
+      break
+    elsif !board.values.include?(" ")
+      prompt_its_a_tie(board)
+      break
     end
   end
 
@@ -253,14 +257,12 @@ loop do
   end
 end
 
-if score[:human] < winning_score.to_i && score[:computer] < winning_score.to_i
-  prompt("You have withdrawn, so the computer wins :D ")
-elsif score.key(winning_score.to_i) == :human
+if score.key(winning_score.to_i) == :human
   prompt "The score is " \
          "Player: #{score[:human]}; Computer: #{score[:computer]}"
   puts ""
   prompt("Congratulations, You have won the match! ;)")
-else
+elsif score.key(winning_score.to_i) == :computer
   prompt "The score is " \
          "Player: #{score[:human]}; Computer: #{score[:computer]}"
   puts ""
