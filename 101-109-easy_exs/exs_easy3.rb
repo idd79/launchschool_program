17797cfa611bfe4958ac7eccc646cd92659fcec6
Ivan Duran <idd79@yandex.com>
2016-10-11 20:21:57 +0300

    Add easy exercises for 101 and 109

--- 101-109-easy_exs/exs_easy3.rb
+++ 101-109-easy_exs/exs_easy3.rb
@@ -12,7 +12,7 @@ def ask_6_numbers
   numbers = []
   NUMBERS.each do |num|
     puts "==> Enter the #{num} number:"
-    numbers << gets.chomp.to_i
+    numbers << STDIN.gets.chomp.to_i
   end
   numbers
 end
@@ -26,3 +26,41 @@ else
 end
 
 next_exc
+
+# Counting the number of characters (Ex. 3)
+
+print 'Please write word or multiple words: '
+string = gets.chomp
+
+chars_count = string.chars.count { |char| char != " " }
+
+puts "There are #{chars_count} characters in \"#{string}\"."
+
+next_exc
+
+# Palindromic Strings (Ex. 8)
+
+def palindrome?(input)
+  input == input.reverse
+end
+
+p palindrome?('madam') == true
+p palindrome?('Madam') == false          # (case matters)
+p palindrome?("madam i'm adam") == false # (all characters matter)
+p palindrome?('356653') == true
+
+next_exc
+
+# Palindrome part 2 (Ex. 9)
+
+def real_palindrome?(string)
+  string = string.downcase.chars.reject { |char| char.match(/[^a-z0-9]/) }
+  palindrome?(string)
+end
+
+p real_palindrome?('madam') == true
+p real_palindrome?('Madam') == true           # (case does not matter)
+p real_palindrome?("Madam, I'm Adam") == true # (only alphanumerics matter)
+p real_palindrome?('356653') == true
+p real_palindrome?('356a653') == true
+p real_palindrome?('123ab321') == false
